af935f77f90b5b131cd4c34199dcd1f06a9bc129
helloanh <ahoang10@jhu.edu>
2018-02-26 23:53:15 -0500

    add app and people loop for practice

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-alert('hello world');
+let people = require('./people.js');
+let $ = require('jquery');
+
+$.each(people, function(key, value){
+	$('body').append('<h1>'+ people[key].name + '</h1>');
+});
+
+
+
+
---
+++ src/js/people.js
@@ -0,0 +1,11 @@
+function getHello(){
+	return 'Hello there';
+};
+
+let people =[
+	{name: 'John Doe'},
+	{name: 'Spock Something'},
+	{name: 'Bobby Vu'}
+];
+module.exports = people;
+
