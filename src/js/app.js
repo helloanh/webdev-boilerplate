c39706eb9479e82e717615e8bb9a2eba39fb00a4
helloanh <ahoang10@jhu.edu>
2018-03-01 16:38:59 -0500

    clean up js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 require('../css/style.css');
-let people = require('./people.js');
 let $ = require('jquery');
+let aframe = require('aframe');
 
-$.each(people, function(key, value){
-	$('body').append('<h1>'+ people[key].name + '</h1>');
-});
+
 
 
 
